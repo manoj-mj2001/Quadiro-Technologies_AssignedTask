ef36a1f6afd503c87bf6f7774fec7af0cace0695
Manoj Tambake <manojtambake1122@gmail.com>
2024-08-11 22:06:19 +0530

    added some stuffs

--- admin/manage_cars.php
+++ admin/manage_cars.php
@@ -99,6 +99,21 @@ $cars = isset($_SESSION['cars']) ? $_SESSION['cars'] : [];
     </style>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Quadiro Tech</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a id="userLoginBtn" class="btn btn-outline-light" href="../user/user_login.php">See Cars in User Section</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="container car-management-container">
         <h1 class="text-center">Manage Cars</h1>
         <form method="POST">
